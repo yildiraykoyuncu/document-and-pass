8483dc794510219ce19ca01471c0dcc408958eb4
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-07-14 21:44:18 +0200

    completed 2-palindroms and 3-caesar

--- exercises/2-palindromes/index.js
+++ exercises/2-palindromes/index.js
@@ -1,8 +1,25 @@
 /**
- *
+ * Checks a string and return if its palindrome or not
+ * @param {string} str string to check
+ * @returns {boolean} it returns true or false
+ * 
+ * @example
+ * 
+ * const check = palindromes('race');
+ * console.log(check); // false
  */
-const palindromes = () => {
+const palindromes = (str) => {
+
+    if (typeof str !== 'string') { throw new TypeError('This is not a string') }
+
+    let temp = str.toLowerCase()
+        .match(/[0-9a-z]/gi)
+    let result = temp.every(letter => letter === temp[temp.length - temp.indexOf(letter) - 1]);
+
+    return result;
 
 };
+
+palindromes('Ada')
 
 module.exports = palindromes;
--- exercises/3-caesar/index.js
+++ exercises/3-caesar/index.js
@@ -1,8 +1,39 @@
 /**
- *
+ * Encrypts strings by shifting every letter by a given number
+ * @param {'string'}
+ * @param {'number'}
+ * @returns {'string'}
  */
-const caesar = () => {
+const caesar = (str, key) => {
 
+    let result = '';
+
+    if (key < 0) {
+        key += 26
+    }
+
+
+    for (let i = 0; i < str.length; i++) {
+        const regex = RegExp(/[A-Za-z]/gi)
+        if (regex.test(str[i])) {
+
+            if (str.codePointAt(i) >= 65 && str.codePointAt(i) <= 90) {
+                result += `${String.fromCodePoint((str.codePointAt(i) - 65 + key)%26 +65)}`
+            } else if (str.codePointAt(i) >= 97 && str.codePointAt(i) <= 122) {
+                result += `${String.fromCodePoint((str.codePointAt(i) - 97 + key)%26 +97)}`
+            }
+
+
+        } else {
+            result += `${str[i]}`
+        }
+    }
+
+
+
+    return result;
 };
+
+
 
 module.exports = caesar;
